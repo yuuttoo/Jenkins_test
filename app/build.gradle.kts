5d172c1a9f6cf99c7699d15659d89afb26671a12
Cheng-Yu Chung <chengyu.chung@gosaico.com>
2024-11-03 14:51:55 +0800

    update compileSDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.jenkins_test"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.jenkins_test"
